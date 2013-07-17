9305788f3ed2c8698dae2c31015dd368d291a58b
Weida Zhang <weidazh9@gmail.com>
2013-07-17 19:56:03 +0800

    clean landing page code. tune the css.

--- js/landing.js
+++ js/landing.js
@@ -1,203 +1,185 @@
-var spinner_count = 0;
-var spinner_timer;
-var spinner_degree = 0;
-function spinner_spin() {
-    // For webkit browsers: e.g. Chrome
-    $("#spinner").css('WebkitTransform', 'rotate(' + spinner_degree + 'deg)');
-    // For Mozilla browser: e.g. Firefox
-    $("#spinner").css('-moz-transform', 'rotate(' + spinner_degree + 'deg)');
-    
-    spinner_timer = setTimeout(function() {
-	spinner_degree += 15;
-	spinner_spin();
-    }, 30);
+function LandingSpinner (target) {
+    // http://fgnass.github.io/spin.js/
+    var size = 20;
+    console.log(size);
+    var opts = {
+	lines: 9, // The number of lines to draw
+	length: size / 4, // The length of each line
+	width: 2, // The line thickness
+	radius: size / 5, // The radius of the inner circle
+	corners: 0.8, // Corner roundness (0..1)
+	rotate: 0, // The rotation offset
+	direction: 1, // 1: clockwise, -1: counterclockwise
+	color: '#000', // #rgb or #rrggbb
+	speed: 1, // Rounds per second
+	trail: 60, // Afterglow percentage
+	shadow: false, // Whether to render a shadow
+	hwaccel: false, // Whether to use hardware acceleration
+	className: 'spinner', // The CSS class to assign to the spinner
+	zIndex: 1, // The z-index (defaults to 2000000000)
+	top: 'auto', // Top position relative to parent in px
+	left: 'auto' // Left position relative to parent in px
+    };
+    var spinner = new Spinner(opts);
 
+    var spinner_count = 0;
+    function spinner_inc() {
+	if (spinner_count == 0) {
+	    $(target).show();
+	    spinner.spin(target);
+	}
+	spinner_count ++;
+    }
+
+    function spinner_dec() {
+	spinner_count --;
+	if (spinner_count == 0) {
+	    $(target).hide();
+	    spinner.stop(target);
+	}
+    }
+    this.inc = spinner_inc;
+    this.dec = spinner_dec;
+    this.spinner = spinner;
+    this._target = target;
+    return this;
 }
-function spinner_inc() {
-    if (spinner_count == 0) {
-	$("#spinner").show();
-	spinner_spin();
-    }
-    spinner_count ++;
-}
 
-function spinner_dec() {
-    spinner_count --;
-    if (spinner_count == 0) {
-	$("#spinner").hide();
-	clearTimeout(spinner_timer);
-    }
-}
+function LLGeocoder(spinner, submit_callback) {
+    var location_pick = document.getElementById("arrow");
+    var location_input = document.getElementById("location");
+    var submit_button = document.getElementById("find");
 
-window.addEventListener("load", function load() {
-    function modify_location(name) {
-        document.getElementById("location").value = name;
+    var p0 = { };
+    var selected_ll;
+    function debug(text) {
+	$("#debug-layer").text(text);
     }
-    var ll = undefined;
-    function location_callback(position) {
-        window.gp = position;
-        ll = position.coords.latitude + "," + position.coords.longitude;
-        console.log("location is changed to " + ll);
-        document.getElementById("location").value = ll;
-
-        search_foursquare_name_by_ll(ll, modify_location);
+    function disbale_pick() {
+	$(location_pick).css("opacity", 0);
     }
-    document.getElementById("arrow").addEventListener("click", function() {
-	 if (navigator && navigator.geolocation) {
-            console.log("fetching location");
-            navigator.geolocation.getCurrentPosition(location_callback);
-        }
-        else{
-            // LL unchanged.
-            console.log("cannot get your location");
-        }
-    });
-    document.getElementById("find").addEventListener("click", function() {
-        if (ll) {
-            document.location = 
-                "/new?ll=" + ll;
-        }
-        else {
-            search_ll_from_address(document.getElementById("location").value,
-                function (l, obj) {
-                    document.location = 
-                         "/new?near=" + encodeURI(l)
-                    document.getElementById("location").value = l;
-                    $("#error-warning").css("opacity", "0");
-                },
-                function () {
-                    $("#error-warning").css("opacity", "1");
-                });
-        }
-    });
-
-    var suggestcompletion = "search"; // instead of suggestCompletion, because the latter failed to restrict in radius
-    var minivenues = "venues"; // instead of "minivenues";
-    var AND_FOOD_ID = ""; // the user does not need to be eating some food.
-    function callback_foursquare(data) {
-        var a_foursquare = [];
-        $(data.response[minivenues]).each(function(i, minivenue) {
-            var name = minivenue.name + " @ " + minivenue.location.address;
-            var name_addr = minivenue.name + " @ " + minivenue.location.address + " (" + minivenue.location.distance + "m)";
-            a_foursquare.push({
-                "label": name,
-                "value": name,
-                "type": "foursquare",
-                "obj": minivenue
-            });
-        });
-        return a_foursquare;
+    function disable_submit() {
+	$(submit_button).attr("disabled", true).removeClass("enabled").addClass("disabled");
     }
-    function dynamic_source_foursquare(request, response) {
-        var q0 = request.term;
-        var q = encodeURI(q0);
-        if (q0.length >= 1) {
-            if (ll) {
-                spinner_inc();
-                $.ajax({
-                    url: "https://api.foursquare.com/v2/venues/" + suggestcompletion + "?" + ll + AND_FOOD_ID + "&radius=1000&limit=10&query=" + q + "&" + NOLOGIN_SUFFIX
-                }).done(function(data) {
-                    spinner_dec();
-                    response(callback_foursquare(data));
-                }).fail(function() {
-                    spinner_dec();
-                    console.log("ERROR");
-                    response([]);
-                });
-            }
-            else {
-                spinner_inc();
-                search_ll_from_address(q0,
-                    function(name, obj) {
-                        spinner_dec();
-                        response([{
-                            'label': name,
-                            'value': name,
-                            'type': 'geocode',
-                            'obj': obj
-                        }]);
-                    },
-                    function() {
-                        spinner_dec();
-                        response([]);
-                    });
-            }
-        }
-        else {
-            response([]);
-        }
+    function enable_submit(ll) {
+	if (ll) {
+	    selected_ll = ll;
+	    $(submit_button).attr("disabled", false).removeClass("disabled").addClass("enabled");
+	}
+	else if (selected_ll) {
+	    $(submit_button).attr("disabled", false).removeClass("disabled").addClass("enabled");
+	}
+    }
+    function disable_input() {
+	$(location_input).attr("disabled", true);
+    }
+    function enable_input() {
+	$(location_input).attr("disabled", false);
     }
     function dynamic_source_gmaps(request, response) {
         var q0 = request.term;
         var q = encodeURI(q0);
+	var url;
+	$("#find").attr("disabled", true);
         if (q0.length >= 1) {
-	    $.ajax({
-		url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + q + "&sensor=false"
-	    }).done(function(data) {
-		var a = []
-		$(data.results).each(function(i, obj) {
-		    a.push({
-			"label": obj.formatted_address,
-			"value": obj.formatted_address,
-			"type": "gmaps",
-			"obj": obj,
-		    });
+	    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + q + "&sensor=false";
+	}
+	else {
+	    url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + p0.lat + "," + p0.lng + "&sensor=false"
+	}
+	spinner.inc();
+	$.ajax({
+	    url: url
+	}).done(function(data) {
+	    spinner.dec();
+	    var a = []
+	    $(data.results).each(function(i, obj) {
+		a.push({
+		    "label": obj.formatted_address,
+		    "value": obj.formatted_address,
+		    "type": "gmaps",
+		    "obj": obj,
 		});
-		response(a);
-	    }).fail(function() {
-		console.log("GOOGLE ERROR");
-		response([]);
 	    });
-	}
-    }
-    function dynamic_source(request, response) {
-	var af = [];
-	var ag = [];
-	var has_foursquare = false;
-	function do_response() {
-	    response(ag.concat({
-		'label': '------',
-		'value': '------',
-		'type': 'separator',
-	    }).concat(af));
-	}
-	if (has_foursquare) {
-	    dynamic_source_foursquare(request, function (a) {
-		af = a;
-		do_response();
-	    });
-	}
-	dynamic_source_gmaps(request, function (a) {
-	    ag = a;
-	    if (has_foursquare)
-		do_response();
-	    else
-		response(a);
+	    response(a);
+	    enable_input();
+	}).fail(function() {
+	    spinner.dec();
+	    console.log("GOOGLE ERROR");
+	    response([]);
 	});
     }
+    function dynamic_source(request, response) {
+	return dynamic_source_gmaps(request, response);
+    }
     function dynamic_source_select(_event, ui) {
-        if (ui.item.type == "foursquare") {
-            $("#location").val(ui.item.obj.name);
-            // $("#addr").val(ui.item.obj.location.address);
-            _event.preventDefault();
-        }
-        else if (ui.item.type == "gmaps") {
-            $("#location").val(ui.item.obj.formatted_address);
-            // $("#addr").val(ui.item.obj.location.address);
-            _event.preventDefault();
+        if (ui.item.type == "gmaps") {
+            $(location_input).val(ui.item.value);
+	    var ll = ui.item.obj.geometry.location.lat + "," + ui.item.obj.geometry.location.lng;
+	    debug("select ll = " + ll);
+	    enable_submit(ll);
         }
         else if (ui.item.type == "geocode") {
-            $("#location").val(ui.item.value);
-            _event.preventDefault();
+            $(location_input).val(ui.item.value);
         }
         else {
-            _event.preventDefault();
+        }
+	_event.preventDefault();
+    }
+
+    function location_pick_callback(position) {
+        var ll = position.coords.latitude + "," + position.coords.longitude;
+        debug("pick ll = " + ll);
+
+        // geocode_ll(ll, ll_geocode_fetched, ll_geocode_failed);
+	p0 = { "lat": position.coords.latitude, "lng": position.coords.longitude };
+	$(location_input).autocomplete("search", "");
+    }
+    function location_pick_clicked() {
+	if (navigator && navigator.geolocation) {
+	    disable_input();
+	    disable_submit();
+            debug("fetching location");
+            navigator.geolocation.getCurrentPosition(location_pick_callback);
+        }
+        else{
+	    enable_input();
+	    enable_submit();
+	    disable_pick();
         }
     }
 
-    $("#location").autocomplete({
+    $(location_pick).click(location_pick_clicked);
+    $(location_input).autocomplete({
         source: dynamic_source,
         minLength: 0,
         select: dynamic_source_select,
     });
+    $(submit_button).click(function (_event, ui){
+	debug("submitting");
+	submit_callback(selected_ll);
+    });
+
+    return this;
+}
+
+var landingPage;
+function LandingPage() {
+    var landingSpinner;
+    var llGeocoder;
+    function run() {
+        landingSpinner = new LandingSpinner(document.getElementById("spinner"));
+        llGeocoder = new LLGeocoder(landingSpinner, function (ll) {
+	    document.location = "/new?ll=" + ll;
+	});
+	this.landingSpinner = landingSpinner;
+	this.llGeocoder = llGeocoder;
+    }
+    this.run = run;
+    return this;
+}
+
+var landingPage = new LandingPage();
+window.addEventListener("load", function load() {
+    landingPage.run();
 }, false);
