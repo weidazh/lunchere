c0887a498a085260a8251f7c89de48283c94d796
Weida Zhang <weidazh9@gmail.com>
2013-07-24 17:36:25 +0800

    quick fix: force git tag

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ if git diff --exit-code >/dev/null && git diff --cached --exit-code >/dev/null; 
 	git log -1 > product.deploy.log
 	sed 's/\<dev\>/product/g' app.dev.yaml > app.yaml
 	if appcfg.py update ./ --oauth2; then
-	    git tag product
+	    git tag -f product
 	fi
     else
 	git log -1 > dev.deploy.log
 	cp -a app.dev.yaml app.yaml
 	if appcfg.py update ./ --oauth2; then
-	    git tag dev
+	    git tag -f dev
 	fi
     fi
 else
