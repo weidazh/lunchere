8c1201d971e530a31957c08261b7009f03ac21a2
Weida Zhang <weidazh9@gmail.com>
2013-07-24 17:35:06 +0800

    tag in the git history when deploying

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,15 @@ if git diff --exit-code >/dev/null && git diff --cached --exit-code >/dev/null; 
     if [ "$1" = "product" ]; then
 	git log -1 > product.deploy.log
 	sed 's/\<dev\>/product/g' app.dev.yaml > app.yaml
-	exec appcfg.py update ./ --oauth2
+	if appcfg.py update ./ --oauth2; then
+	    git tag product
+	fi
     else
 	git log -1 > dev.deploy.log
 	cp -a app.dev.yaml app.yaml
-	exec appcfg.py update ./ --oauth2
+	if appcfg.py update ./ --oauth2; then
+	    git tag dev
+	fi
     fi
 else
     echo >&2 "Please make sure all are commited."
